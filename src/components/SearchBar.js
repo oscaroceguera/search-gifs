95920c4f5c305473dc3c23925b0ac3b0e7d53f3f
oscaroceguera <oscaroceguera@gmail.com>
2017-03-23 01:42:54 -0600

    REDUX

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,7 @@
 import React, {Component} from 'react'
 
 class SearchBar extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      term: ''
-    }
-  }
   onInputChange (term) {
-    this.setState({term})
     this.props.onTermChange(term)
   }
   render () {
---
+++ src/containers/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as Actions from '../actions';
+import SearchBar from '../components/SearchBar';
+import GifList from '../components/GifList';
+import GifModal from '../components/GifModal';
+
+import '../styles/app.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar
+          onTermChange={ this.props.requestGifs }
+        />
+        <GifList
+          gifs={ this.props.gifs }
+          onGifSelect={ selectedGif => this.props.openModal({selectedGif}) }
+        />
+        <GifModal
+          modalIsOpen={ this.props.modalIsOpen }
+          selectedGif={ this.props.selectedGif }
+          onRequestClose={ () => this.props.closeModal() }
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    gifs: state.gifs.data,
+    modalIsOpen: state.modal.modalIsOpen,
+    selectedGif: state.modal.selectedGif
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    ...Actions
+  }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
